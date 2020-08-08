a271883ed1ad1498975c127934d8f76c7928a559
piskun-beep <piskun8828@outlook.com>
2020-08-08 13:07:14 -0700

    1 file modified

--- src/app/com1/com1.component.ts
+++ src/app/com1/com1.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-com1',
   templateUrl: './com1.component.html',
-  styleUrls: ['./com1.component.css']
+  styleUrls: ['./com1.component.css'],
+  template: `<h2>Page Title is com1</h2>`
 })
 export class Com1Component {
-  constructor() { }
+  title = 'One Component Title';
+ 
+  constructor(private titleService:Title){
+  }
+ 
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 }
