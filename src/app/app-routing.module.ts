b3db4a774e7cd9fce11c28d7a24a1b3bd3e27308
piskun-beep <piskun8828@outlook.com>
2020-08-08 13:12:32 -0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { Com1Component } from './com1/com1.component'
 import { Com2Component } from './com2/com2.component'
  
- 
 const routes: Routes = [
   {path: 'com1', component:Com1Component},
   {path: 'com2', component:Com2Component}
